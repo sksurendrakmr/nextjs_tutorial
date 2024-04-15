db7806cd2302cf514895a72ad8b1044b35ef77aa
sksurendrakmr <sksurendrakmr@gmail.com>
2024-04-15 22:34:15 +0530

    Route Metadata - static Metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,10 @@
+
+//static metadata
+//This page metadata takes preceeded from root layout metadata.
+export const metdata = {
+  title: "Nextjs tutorial"
+}
+
 const About = () => {
   return (
     <div>About me</div>
