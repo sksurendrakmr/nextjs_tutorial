3cea9afb6c0b7ca8bee34cd317741b41cd7c9d0b
sksurendrakmr <sksurendrakmr@gmail.com>
2024-04-15 22:59:13 +0530

    title metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,15 @@
-import React from 'react'
+import { Metadata } from "next"
 
+const metadata: Metadata = {
+  title: "Blog"
+};
+//o/p of Blog title - Blog | DynamicTitle
+
+// const metadata: Metadata = {
+//   title: {
+//     absolute: "Blog" //The dynamic template from root layout will be ignored.
+//   }
+// };
 const Blog = () => {
   return (
     <div>My Blog</div>
