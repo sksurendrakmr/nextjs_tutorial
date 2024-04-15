30abe5e7b757b7882e0dfc9746b512e127a17ae3
sksurendrakmr <sksurendrakmr@gmail.com>
2024-04-15 22:44:28 +0530

    Routing metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,4 +1,33 @@
-const ProductDetails = ({ params }: {params: {productId:string}}) => {
+import { Metadata } from "next"
+
+type Props = {
+  params: {
+    productId: string;
+  };
+}
+
+/**
+ * Note: generateMetadata() can also be defined as async function 
+ *
+ */
+// export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+//   const title = await new Promise(resolve => {
+//     setTimeout(() => {
+//       resolve(`Iphone ${params.productId}`)
+//     }, 100)
+//   })
+//   return {
+//     title: `Product ${title}`
+//   }
+// }
+
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `Product ${params.productId}`
+  }
+}
+
+const ProductDetails = ({ params }: Props) => {
   return (
     <div>ProductDetails - {params.productId}</div>
   )
