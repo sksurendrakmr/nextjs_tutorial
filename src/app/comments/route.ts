2c943dfacf9a8076bb0c246e69c21f050c6d2778
sksurendrakmr <sksurendrakmr@gmail.com>
2024-04-21 00:27:25 +0530

    Handling POST Request

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -2,3 +2,21 @@ import { comments } from "./data";
 export async function GET() {
   return Response.json(comments);
 }
+/**
+ * Every route handler function receives the standard web "request" object as parameter
+ *
+ */
+export async function POST(request: Request) {
+  const comment = await request.json();
+  const newComment = {
+    id: comments.length + 1,
+    text: comment.text,
+  };
+  comments.push(newComment);
+  return new Response(JSON.stringify(newComment), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    status: 201,
+  });
+}
