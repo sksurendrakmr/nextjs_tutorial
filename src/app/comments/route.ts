8ac9367ba450b1d4031eadfbed368da4b02f881f
sksurendrakmr <sksurendrakmr@gmail.com>
2024-04-21 01:08:26 +0530

    URL query parameters

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,6 +1,18 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data";
-export async function GET() {
-  return Response.json(comments);
+/**
+ * To handle the query parameter effectively, we need the request parameter.
+ * NextRequest ->NextRequest object provides convenient methods for managing query parameters with ease.
+ *
+ * Useful for sort, filter and pagination for the data.
+ */
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams; //access all query parameters
+  const query = searchParams.get("query"); //get individual query
+  const filteredComments = query
+    ? comments.filter((comment) => comment.text.includes(query))
+    : comments;
+  return Response.json(filteredComments);
 }
 /**
  * Every route handler function receives the standard web "request" object as parameter
