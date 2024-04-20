b0b8b1b14e389e12292fdb13c8ec673e98a4bdca
sksurendrakmr <sksurendrakmr@gmail.com>
2024-04-21 03:29:47 +0530

    cookies and headers manipulation using middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,26 @@
 import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  //Requirement is redirect the user to homepage if /hello is accessed
-  if (request.nextUrl.pathname === "/profile") {
-    return NextResponse.redirect(new URL("/hello", request.url));
+  const response = NextResponse.next();
+
+  const themePreference = request.cookies.get("theme");
+  if (!themePreference) {
+    response.cookies.set("theme", "dark");
   }
+
+  response.headers.set("custom-header", "custome-value");
+  return response;
 }
 
 /**
  * Apart from handling redirections, middleware also allows URL rewrites which is
  * useful tool for legacy URL support or SEO optimization.
+ */
+
+/**
+ * Cookie and Headers in Middleware
+ * ---
+ * Requirement
+ * --
+ * Modify the middleware to handle user preferences for themes and add a custom header for all responses.
  */
